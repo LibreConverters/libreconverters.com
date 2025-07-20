354e0e1838d2a6bd6ae29a2befff990d379e9e74
Mike Wyatt <mikewyxtt@gmail.com>
2025-07-20 10:08:09 -0400

    fix github pages deployment issues stemming from missing config values

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/libreconverters.com',
+  assetPrefix: '/libreconverters.com',
+  trailingSlash: true,
+  
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
