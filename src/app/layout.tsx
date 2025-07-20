ae0ede9cb7dc21a577a0beaf41d14b9cc0b5c3ca
Mike Wyatt <mikewyxtt@gmail.com>
2025-07-20 10:22:08 -0400

    script shouldnt be self closed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,10 +43,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script 
+        <script
           async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9181884142549970"
-          crossOrigin="anonymous" 
-        />
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
