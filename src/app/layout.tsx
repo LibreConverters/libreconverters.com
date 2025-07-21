70c552d5c4f83650f327017b7534511821bdadbf
Mike Wyatt <mikewyxtt@gmail.com>
2025-07-20 20:02:20 -0400

    google still cant find the page, trying this other method..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,10 +43,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script
-          async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9181884142549970"
-          crossOrigin="anonymous"
-        ></script>
+        <meta name="google-adsense-account" content="ca-pub-9181884142549970"/>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
